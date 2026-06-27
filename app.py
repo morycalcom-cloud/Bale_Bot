7325db4b88a8eb8d160033841ce5614148aeda8d
morycalcom-cloud <morycal.com@gmail.com>
2026-06-27 17:44:10 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,70 +1,49 @@
-import os
-import requests
-from flask import Flask, request, jsonify
+from flask import Flask, request
+
+from handlers.start import start
+from handlers.messages import echo
+from handlers.admin import admin
 
 app = Flask(__name__)
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-if not BOT_TOKEN:
-    raise Exception("BOT_TOKEN environment variable not found")
-
-API = f"https://tapi.bale.ai/bot{BOT_TOKEN}"
-
-
-def send_message(chat_id, text):
-    requests.post(
-        f"{API}/sendMessage",
-        json={
-            "chat_id": chat_id,
-            "text": text
-        },
-        timeout=10
-    )
 
 
 @app.route("/")
 def home():
-    return "Bale Bot is Running!"
+    return "ONLINE"
 
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    update = request.get_json()
 
-    print(update)
+    update = request.json
 
     if not update:
-        return jsonify({"ok": True})
+        return "OK"
 
-    if "message" in update:
+    if "message" not in update:
+        return "OK"
 
-        message = update["message"]
+    message = update["message"]
 
-        chat_id = message["chat"]["id"]
+    text = message.get("text", "")
 
-        text = message.get("text", "")
+    if text == "/start":
+        start(message)
 
-        if text == "/start":
-            send_message(
-                chat_id,
-                "سلام 👋\nربات با موفقیت روی Railway اجرا شد."
-            )
+    elif text == "/admin":
+        admin(message)
 
-        elif text == "/ping":
-            send_message(chat_id, "🏓 Pong!")
+    else:
+        echo(message)
 
-        else:
-            send_message(
-                chat_id,
-                f"شما نوشتید:\n\n{text}"
-            )
-
-    return jsonify({"ok": True})
+    return "OK"
 
 
 if __name__ == "__main__":
+
+    import os
+
     app.run(
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8080))
+        port=int(os.getenv("PORT", 8080))
     )
